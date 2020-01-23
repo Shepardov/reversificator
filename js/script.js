64188b7e95aa4afbc364e42f4ca6f843209f5689
Shepardov <36133103+Shepardov@users.noreply.github.com>
2020-01-23 16:28:05 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let phrase = document.getElementById('phrase');
+let phraseToDivide = document.getElementById('phrase-divide');
+let reverse = document.getElementById('reverse');
+let result = document.getElementById('result');
+
+
+phrase.addEventListener('input', () => {
+    phraseToDivide.innerText = phrase.value;
+});
+
+phraseToDivide.addEventListener('mouseover', () => {
+    let words = phraseToDivide.innerText.split(' ');
+    words.forEach(elem => {
+
+    });
+});
+
+reverse.addEventListener('click', () => {
+    let words = phraseToDivide.innerText.split(' ');
+    result.innerText = "";
+    words.forEach(word => {
+        reverseWord(word);
+    });
+});
+
+function reverseWord(word) {
+    if(word.length < 4)
+    {
+        result.innerHTML += `${word + word.split('').reverse().join('').slice(1)} `;       
+        return;
+    }
+    let divideCount = Math.floor(word.length / 2);
+    let firstHalf = word.slice(0, divideCount);
+    let secondHalf = word.slice(divideCount, word.length);
+    let firstReversedWord = firstHalf + firstHalf.split('').reverse().join('').slice(1);
+    let secondReversedWord = secondHalf.split('').reverse().join('') + secondHalf.slice(1);
+    result.innerHTML += `${firstReversedWord} или ${secondReversedWord} `;
+}
